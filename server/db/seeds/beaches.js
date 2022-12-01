80e52054a7d8ae2340f5d4b39fadf365291bc7e0
paola-santos <56907621+pola82@users.noreply.github.com>
2022-12-01 16:13:45 +1300

    fixed seeds

--- server/db/seeds/beaches.js
+++ server/db/seeds/beaches.js
@@ -7,7 +7,8 @@ exports.seed = async function(knex) {
       id: 1,
       name: 'Lyall bay',
       description: 'Lyall Bay is a bay and suburb on the south side of the Rongotai isthmus in Wellington, New Zealand. The bay is a popular surf beach, featuring a breakwater at the eastern end.',
-      coordinates: '41.3291, 174.7953',
+      latitude: '41.3291',
+      longitude: '174.7953',
 
 
     },
@@ -15,8 +16,8 @@ exports.seed = async function(knex) {
       id: 2,
       name: 'Princess bay',
       description: 'Princess Bay is one of the southern beaches of Wellington, New Zealand.',
-      coordinates: '41.3438, 174.7817',
-     
+      latitude: '41.3438',
+      longitude: '174.7817'
 
 
     },
@@ -24,8 +25,8 @@ exports.seed = async function(knex) {
       id: 3,
       name: 'Oriental bay',
       description: 'Oriental Bay is a bay and suburb of Wellington, the capital city of New Zealand. Noted for being both a popular beach and a luxurious centre of affluence in the city, it is located close to the Central Business District on Wellington Harbour.',
-      coordinates: '41.2916, 174.7929',
-     
+      latitude: '41.2916',
+      longitude: '174.7929'
 
 
     },
@@ -33,8 +34,8 @@ exports.seed = async function(knex) {
       id: 4,
       name: 'Scorching bay',
       description: 'This popular sandy inner-harbour bathing beach has a large grassed area and is sheltered from the northerly wind. It is a great place to soak up the sun and watch ships entering and leaving the harbour.',
-      coordinates: '41.2972, 174.8333',
-    
+      latitude: '41.2972',
+      longitude: '174.8333'
 
     },
   ]);
