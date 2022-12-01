b57fa00b6a8dc22fdee6ab0461da5ed9a5a1df41
paola-santos <56907621+pola82@users.noreply.github.com>
2022-12-02 10:54:06 +1300

    fixed name

--- client/actions/index.js
+++ client/actions/index.js
@@ -2,7 +2,7 @@ import { getAllBeaches } from '../apis/beaches'
 
 export const GET_BEACHES = 'GET_BEACHES'
 
-export function getAllBeaches(beaches){
+export function getAllBeachesAction(beaches){
   return {
     type: GET_BEACHES,
     payload: beaches
