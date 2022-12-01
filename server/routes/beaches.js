05b16af9af77b60eca7e8e5378699f050ec90f30
Kelsey Paterson <kelseypaterson72@gmail.com>
2022-12-01 15:27:48 +1300

    add beach routes

---
+++ server/routes/beaches.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const db = require('../db/beaches')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  db.getAllBeaches()
+    .then((results) => {
+      res.json(results)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  db.getBeachById(id)
+    .then((result) => {
+      res.json(result)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Something went wrong' })
+    })
+})
+
+module.exports = router
