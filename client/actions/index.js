7d7f1207b27d9a62cc364c23d8821739d12cb77d
paola-santos <56907621+pola82@users.noreply.github.com>
2022-12-02 10:46:59 +1300

    created allBeachesAction

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,5 +1,16 @@
 import { getAllBeaches } from '../apis/beaches'
 
+export const GET_BEACHES = 'GET_BEACHES'
+
+export function getAllBeaches(beaches){
+  return {
+    type: GET_BEACHES,
+    payload: beaches
+    
+  }
+
+}
+
 
 export function getAllBeachesThunk() {
   return (dispatch) => {
