2ad0e5e59b6e7f581a8ecc3e5734ab7622073e14
paola-santos <56907621+pola82@users.noreply.github.com>
2022-12-02 16:54:27 +1300

    setup routes in app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 import BeachList from './BeachList'
 import BeachDetails from './BeachDetails'
+import BeachCard from './BeachCard'
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <h1>Welly on a Beach</h1>
+//       <BeachList />
+//     </div>
+//   )
+// }
 
 function App() {
   return (
-    <div className="app">
-      <h1>Welly on a Beach</h1>
-      <BeachList />
-    </div>
+    <>
+      {/* <Nav/> */}
+      <Routes>
+        <Route path='/' element={<BeachList/>}/>
+        <Route path='/beachCard' element={<BeachCard/>}/>
+        <Route path='/beachDetails/:id' element={<BeachDetails/>}/>
+      </Routes>
+    </>
   )
 }
 
--- client/index.js
+++ client/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </Provider>,
     document.getElementById('app')
   )
