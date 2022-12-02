35bde92e286559b243f2818b4751f942ea71df74
Kelsey Paterson <kelseypaterson72@gmail.com>
2022-12-02 18:30:21 +1300

    fixed wind reducer

--- client/reducers/windReducer.js
+++ client/reducers/windReducer.js
@@ -1,14 +1,12 @@
 import { GET_BEACH_WIND } from '../actions'
 
-const initialState = []
+const initialState = {}
 
 const reducer = (state = initialState, action) => {
   const { type, payload } = action
-  const id = payload.id
-  const windData = payload.windData
   switch (type) {
     case GET_BEACH_WIND:
-      return [...state, { [id]: windData }]
+      return { ...state, [payload.id]: payload.windData }
     default:
       return state
   }
