b991d6015d775ef5e090e1f2391101dac04e04b5
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-12-03 15:41:01 +1300

    Add MUI card component to BeachCard

--- client/components/BeachCard.jsx
+++ client/components/BeachCard.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 
+import { Card, CardContent, CardMedia, CardActionArea } from '@mui/material'
+
 function BeachCard(props) {
   const { id, name } = props.beach
 
   return (
     <div>
-      <li key={id}>
-        <p>{name}</p>
-        <p>Placeholder for wind speed.</p>
-        <p>Placeholder for wind direction.</p>
-        <img
-          src={`/images/${id}.jpg`}
-          alt={`${name} on a summer day.`}
-          width={'500'}
-          height={'300'}
-        />
-      </li>
+      <Card sx={{ maxWidth: 345 }} key={id}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="300"
+            image={`/images/${id}.jpg`}
+            alt={`${name} on a summer day.`}
+          />
+          <CardContent sx={{ boxShadow: 1 }}>
+            <p>{name}</p>
+            <p>Placeholder for wind speed.</p>
+            <p>Placeholder for wind direction.</p>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </div>
   )
 }
