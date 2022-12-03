9f84fcb66023019246a33ee8f7434432c9b709b0
Kelsey Paterson <kelseypaterson72@gmail.com>
2022-12-03 13:55:26 +1300

    replaced api calls with hard coded data

--- client/components/BeachCard.jsx
+++ client/components/BeachCard.jsx
@@ -6,11 +6,32 @@ import { getTime } from '../utils'
 function BeachCard(props) {
   const { id, name, latitude, longitude } = props.beach
   const dispatch = useDispatch()
-  const windData = useSelector((store) => store.windData)
+
+  // UNCOMMENT BELOW AND DELETE HARD CODED DATA AFTER DEVELOPMENT COMPLETE.
+  // const windData = useSelector((store) => store.windData)
+  const windData = {
+    1: {
+      windSpeed: 4.5766306,
+      windDirection: 358.4376,
+    },
+    2: {
+      windSpeed: 4.856751,
+      windDirection: 357.97202,
+    },
+    3: {
+      windSpeed: 4.0311227,
+      windDirection: 359.65985,
+    },
+    4: {
+      windSpeed: 3.9822397,
+      windDirection: 0.53644973,
+    },
+  }
 
   useEffect(() => {
     const time = getTime()
-    dispatch(getBeachWindDataThunk(latitude, longitude, time, id))
+    // UNCOMMENT BELOW AFTER DEVELOPMENT COMPLETE: Dispatch commented out to minimise api calls and replaced with hard coded data.
+    // dispatch(getBeachWindDataThunk(latitude, longitude, time, id))
   }, [])
 
   return (
