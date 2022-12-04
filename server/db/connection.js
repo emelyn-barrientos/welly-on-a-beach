f52d349dbc9de0b066f1a5e474555941b955ec16
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-12-04 15:33:31 +1300

    Fix issue with connection file for knex module

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,10 @@
-const env = process.env.NODE_ENV || 'development'
-const knex = require('knex')
-const config = require('./knexfile')[env]
-const connection = knex(config[env])
+// const env = process.env.NODE_ENV || 'development'
+// const knex = require('knex')
+// const config = require('./knexfile')[env]
+// const connection = knex(config[env])
 
-module.exports = connection
+const environment = process.env.NODE_ENV || 'development'
+const config = require('./knexfile')[environment]
+const conn = require('knex')(config)
+
+module.exports = conn
