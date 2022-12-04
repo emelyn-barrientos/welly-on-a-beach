78e04561ec91065e3d594bb1f96bbbfee89b1beb
paola-santos <56907621+pola82@users.noreply.github.com>
2022-12-04 13:34:58 +1300

    fix issue with route PR

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,16 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 
 import BeachList from './BeachList'
 import BeachDetails from './BeachDetails'
-import BeachCard from './BeachCard'
 
-// function App() {
-//   return (
-//     <div className="app">
-//       <h1>Welly on a Beach</h1>
-//       <BeachList />
-//     </div>
-//   )
-// }
 
 function App() {
   return (
@@ -20,8 +11,7 @@ function App() {
       {/* <Nav/> */}
       <Routes>
         <Route path='/' element={<BeachList/>}/>
-        <Route path='/beachCard' element={<BeachCard/>}/>
-        <Route path='/beachDetails/:id' element={<BeachDetails/>}/>
+        <Route path='/beach/:id' element={<BeachDetails/>}/>
       </Routes>
     </>
   )
