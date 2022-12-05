f86f1da81d38d18e9ec548f61c687590770227bc
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-12-05 23:22:21 +1300

    Add themes file to hold app ttheming properties

--- client/styles/themes.js
+++ client/styles/themes.js
@@ -1,31 +1,54 @@
-import { createTheme } from '@mui/system'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 
-export const theme = createTheme({
+const theme = extendTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#273469',
+      light: '#565D97',
+      dark: '#000E3E',
+    },
+    secondary: {
+      main: '#1A96F0',
+      light: '#6BC7FF',
+      dark: '#0069bd',
+      contrastText: '#000e3e',
+    },
+    background: {
+      default: '#fafaff',
+    },
+    text: {
+      primary: '#273469',
+      secondary: '#1A96F0',
+    },
+  },
   typography: {
     fontFamily: 'Open Sans',
     h1: {
-      fontSize: '4rem',
-      fontWeight: 700,
+      fontSize: '3rem',
+      fontWeight: 800,
     },
     h2: {
-      fontSize: '3rem',
-      fontWeight: 600,
+      fontSize: '2.2rem',
+      fontWeight: 800,
     },
     h3: {
-      fontSize: '2rem',
-      fontWeight: 500,
+      fontSize: '1.5rem',
+      fontWeight: 700,
     },
     subtitle1: {
-      fontSize: '1.75rem',
-      fontWeight: 300,
+      fontSize: '1rem',
+      fontWeight: 500,
     },
     body1: {
-      fontSize: '1.5rem',
+      fontSize: '1rem',
       fontWeight: 400,
     },
     body2: {
-      fontSize: '1rem',
+      fontSize: '.8rem',
       fontWeight: 400,
     },
   },
 })
+
+export default theme
