3dddefcb8359ccfeb976a65e0f82ea173432e255
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-12-05 23:23:38 +1300

    Import themes to apply global styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,45 +5,17 @@ import BeachList from './BeachList'
 import BeachDetails from './BeachDetails'
 import WellyWeatherData from './WellyWeatherData'
 import Header from './Header'
+import theme from '../styles/themes'
 
-import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { Experimental_CssVarsProvider as CSSVarsProvider } from '@mui/material/styles'
 import { Container } from '@mui/system'
 
-
 function App() {
-  const theme = createTheme({
-    typography: {
-      fontFamily: 'Open Sans',
-      h1: {
-        fontSize: '4rem',
-        fontWeight: 700,
-      },
-      h2: {
-        fontSize: '3rem',
-        fontWeight: 600,
-      },
-      h3: {
-        fontSize: '2rem',
-        fontWeight: 500,
-      },
-      subtitle1: {
-        fontSize: '1.75rem',
-        fontWeight: 300,
-      },
-      body1: {
-        fontSize: '1.5rem',
-        fontWeight: 400,
-      },
-      body2: {
-        fontSize: '1rem',
-        fontWeight: 400,
-      },
-    },
-  })
-
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
+      <CSSVarsProvider theme={theme}>
+        <CssBaseline />
         <Container maxWidth="lg">
           <Header />
           <WellyWeatherData />
@@ -52,7 +24,7 @@ function App() {
             <Route path="/beach/:id" element={<BeachDetails />} />
           </Routes>
         </Container>
-      </ThemeProvider>
+      </CSSVarsProvider>
     </div>
   )
 }
