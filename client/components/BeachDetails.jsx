aeb7b158c0ad8c82b20727f27fa63a9e0789e4db
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-12-05 23:22:57 +1300

    Slight styling improvements to beach details page

--- client/components/BeachDetails.jsx
+++ client/components/BeachDetails.jsx
@@ -3,8 +3,7 @@ import { useSelector } from 'react-redux'
 
 import { useParams } from 'react-router-dom'
 
-import { Typography } from '@mui/material'
-import { Container } from '@mui/system'
+import { Typography, Box, Container } from '@mui/material'
 
 import HomeButton from './HomeButton'
 
@@ -31,18 +30,27 @@ function BeachDetails() {
   const beach = beaches.find((beach) => beach.id === Number(id))
 
   return (
-    
-    <div>
-      <Container maxWidth="lg">
+    <Container maxWidth="lg">
       <HomeButton />
-        <Typography
-          variant="h3"
-          sx={{
-            marginBlock: 2,
-          }}
-        >
-          {beach.name}
-        </Typography>
+      <Typography
+        variant="h1"
+        sx={{
+          marginBlock: 4,
+          textAlign: 'center',
+        }}
+      >
+        {beach.name}
+      </Typography>
+      <Typography
+        variant="body1"
+        sx={{
+          marginBlock: 4,
+          textAlign: 'center',
+        }}
+      >
+        <strong>Wind speed: </strong>35 kmph
+      </Typography>
+      <Box>
         <Typography
           variant="body1"
           sx={{
@@ -51,17 +59,15 @@ function BeachDetails() {
         >
           {beach.description}
         </Typography>
-        <Typography
-          variant="body1"
-          sx={{
-            marginBlock: 1,
-          }}
-        >
-          <strong>Wind data: </strong>35 kmph
-        </Typography>
-        <img src={`/images/${id}.jpg`} alt={beach.name} width={'90%vw'} />
-      </Container>
-    </div>
+      </Box>
+      <Box
+        component="img"
+        src={`/images/${id}.jpg`}
+        maxWidth="md"
+        alt={beach.name}
+        sx={{ display: 'flex', justifyContent: 'center', marginBlock: 2 }}
+      />
+    </Container>
   )
 }
 
