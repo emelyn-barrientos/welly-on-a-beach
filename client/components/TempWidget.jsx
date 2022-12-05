1ba2f0bd21fab65b111e92530831588cacef61bc
Kelsey Paterson <kelseypaterson72@gmail.com>
2022-12-05 16:22:38 +1300

    styling

--- client/components/TempWidget.jsx
+++ client/components/TempWidget.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 
+import { Card } from '@mui/material'
+
 function TempWidget({ temp, size }) {
   return (
-    <div
-      className="widget-container"
-      style={{ width: `${size}px`, height: `${size}px` }}
-    >
-      <div className="temp-container">
-        <div className="temp-text" style={{ fontSize: `${size * 0.4}px` }}>
-          {temp}
-        </div>
-        <div className="degrees-text" style={{ fontSize: `${size * 0.25}px` }}>
-          °C
+    <Card>
+      <div
+        className="widget-container"
+        style={{ width: `${size}px`, height: `${size}px` }}
+      >
+        <div className="temp-container">
+          <div className="temp-text" style={{ fontSize: `${size * 0.4}px` }}>
+            {temp}
+          </div>
+          <div
+            className="degrees-text"
+            style={{ fontSize: `${size * 0.25}px` }}
+          >
+            °C
+          </div>
         </div>
       </div>
-    </div>
+    </Card>
   )
 }
 
--- client/components/WeatherIconWidget.jsx
+++ client/components/WeatherIconWidget.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { chooseIcon } from '../utils'
 
+import { Card } from '@mui/material'
+
 function WeatherIconWidget({ rainRate, cloudCover, size }) {
   const icon = chooseIcon(rainRate, cloudCover)
   return (
-    <div
-      className="widget-container"
-      style={{ width: `${size}px`, height: `${size}px` }}
-    >
-      <div>{icon}</div>
-    </div>
+    <Card>
+      <div
+        className="widget-container"
+        style={{ width: `${size}px`, height: `${size}px` }}
+      >
+        <div>{icon}</div>
+      </div>
+    </Card>
   )
 }
 
