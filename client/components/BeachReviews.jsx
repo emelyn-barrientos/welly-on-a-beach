ca7dd80468a9d51c9bf831896808f929a13b2826
emelyn-barrientos <emelyn.barrientos22@gmail.com>
2022-12-05 23:34:14 +1300

    Create BeachReviews.jsx component with Material UI imports and React hooks

---
+++ client/components/BeachReviews.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { useParams } from 'react-router-dom'
+
+import { Container } from '@mui/system'
+import { Typography } from '@mui/material'
+
+import ReviewCard from './ReviewCard'
+
+function BeachReviews() {
+  const { id } = useParams()
+
+  const beaches = useSelector((store) => store.beaches)
+
+  const beach = beaches.find((beach) => beach.id === Number(id))
+
+  const reviews = beach.reviews
+
+  return (
+    <div>
+      <Container maxWidth="lg">
+        <Typography
+          variant="body1"
+          sx={{
+            marginBlock: 2,
+          }}
+        >
+          Leave a beach review!
+        </Typography>
+        {reviews &&
+          reviews.map((review) => {
+            return <ReviewCard key={review.id} review={review} />
+          })}
+      </Container>
+    </div>
+  )
+}
+
+export default BeachReviews
