8cc26f747be4980418de5b4b7e4d74617b9fe7ab
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-12-05 18:52:47 +1300

    Add theme and styles

---
+++ client/styles/themes.js
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/system'
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: 'Open Sans',
+    h1: {
+      fontSize: '4rem',
+      fontWeight: 700,
+    },
+    h2: {
+      fontSize: '3rem',
+      fontWeight: 600,
+    },
+    h3: {
+      fontSize: '2rem',
+      fontWeight: 500,
+    },
+    subtitle1: {
+      fontSize: '1.75rem',
+      fontWeight: 300,
+    },
+    body1: {
+      fontSize: '1.5rem',
+      fontWeight: 400,
+    },
+    body2: {
+      fontSize: '1rem',
+      fontWeight: 400,
+    },
+  },
+})
