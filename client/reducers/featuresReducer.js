dcfe8853abb69169e42f1fa4f67337d1e5758a83
Kelsey Paterson <kelseypaterson72@gmail.com>
2022-12-07 07:44:46 +1300

    comment out features reducer, may be deleted after review

--- client/reducers/featuresReducer.js
+++ client/reducers/featuresReducer.js
@@ -1,16 +1,16 @@
-import { GET_BEACH_FEATURES } from '../actions'
+// import { GET_BEACH_FEATURES } from '../actions'
 
-const initialState = []
+// const initialState = []
 
-const reducer = (state = initialState, action) => {
-  const { type, payload } = action
-  switch (type) {
-    // this should be SET rather than get
-    case GET_BEACH_FEATURES:
-      return payload
-    default:
-      return state
-  }
-}
+// const reducer = (state = initialState, action) => {
+//   const { type, payload } = action
+//   switch (type) {
+//     // this should be SET rather than get
+//     case GET_BEACH_FEATURES:
+//       return payload
+//     default:
+//       return state
+//   }
+// }
 
-export default reducer
+// export default reducer
