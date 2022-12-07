3457be5f2ce77577ce34139f2dfad744b1f8e03f
Alyssa Barbieri <19173672+alyssa-barbieri@users.noreply.github.com>
2022-12-07 13:00:09 +1300

    Add link to go home to logo

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 import { WOABLogo } from './Logo'
+import { Link } from 'react-router-dom'
 
 import { Container } from '@mui/material'
 
 function Header() {
   return (
     <Container maxWidth="sm" sx={{ position: 'relative', marginBlock: 8 }}>
-      <WOABLogo />
+      <Link to={'/'}>
+        <WOABLogo alt="Wellington on a beach" title="Wellington on a beach" />
+      </Link>
     </Container>
   )
 }
